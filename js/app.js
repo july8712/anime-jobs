9a6379bcc84e0d520193bc5bef330000dd34bdb8
july8712 <julietabondi@gmail.com>
2022-11-28 18:05:26 -0300

    add 'ver más' functionality in paragraphs

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ const listJobs = (jobs) => {
                 <div class="bg-[url('${image}')] bg-cover bg-center bg-no-repeat w-full h-[200px] mb-7"></div>
                 <h2 class="mb-1 text-2xl">${name}</h2>
                 <h3 class="mb-4">Publicado el: ${publicationDate}</h3>
-                <p class="mb-4">${description}</p>
+                <p class="mb-4">${description.slice(0,70)}...<a href="" class="ml-4 text-[#ce4164]">Ver más</a></p>
                 <span class="bg-[#ce4164] text-white px-3 py-1 mt-1 inline-block rounded-lg">${anime}</span>
                 <span class="bg-[#ce4164] text-white px-3 py-1 mt-1 inline-block rounded-lg">${seniority}</span>
                 <span class="bg-[#ce4164] text-white px-3 py-1 mt-1 inline-block rounded-lg">${location}</span>
