1bdbbffe7f0528a1ff5a5bbcbbfb351d280aca99
july8712 <julietabondi@gmail.com>
2022-12-07 21:19:19 -0300

    View detail functionality

--- js/app.js
+++ js/app.js
@@ -97,6 +97,7 @@ const listJobs = (jobs) => {
                 <span class="bg-[#ce4164] text-white px-3 py-1 mt-1 inline-block rounded-lg">${experience}</span>
                 <span class="bg-[#ce4164] text-white px-3 py-1 mt-1 inline-block rounded-lg">${location}</span>
                 <span class="bg-[#ce4164] text-white px-3 py-1 mt-1 inline-block rounded-lg">Solicitudes: ${applications}</span>
+                <button class="bg-[#b7325e] text-white font-semibold w-full mt-3 py-3 rounded-lg" data-id="${id}" onclick="seeMore('${id}')">Ver más</button>
             </div>
             `
             counter += 1
@@ -127,6 +128,7 @@ const printJobs = (jobs) => {
                 <span class="bg-[#ce4164] text-white px-3 py-1 mt-1 inline-block rounded-lg">${experience}</span>
                 <span class="bg-[#ce4164] text-white px-3 py-1 mt-1 inline-block rounded-lg">${location}</span>
                 <span class="bg-[#ce4164] text-white px-3 py-1 mt-1 inline-block rounded-lg">Solicitudes: ${applications}</span>
+                <button class="bg-[#b7325e] text-white font-semibold w-full mt-3 py-3 rounded-lg" data-id="${id}" onclick="seeMore('${id}')">Ver más</button>
             </div>
             `
             counter += 1
@@ -175,6 +177,42 @@ const locationList = (array) => {
     for (const location of localionSelect) {
         $("#location").innerHTML += `<option value="${location}">${location}</option>`
     }
+}
+
+// function for see more
+
+const seeMore = (numId) => {
+    $("#highlights").style.display = "none"
+    $("#billboard").style.display = "none"
+    $("#jobs").style.display = "none"
+    removeClass($("#details"), "hidden")
+    printDetails(dataJobs.filter(job => job.id === numId)) 
+}
+
+const printDetails = (jobs) => {
+    
+    const { name, description, location, experience, salary, publicationDate, image, anime, page, applications, id } = jobs[0]
+
+    $("#details").innerHTML = `
+        <div class="w-3/5 bg-white p-10 flex border-2">
+                <div class="bg-[url('${image}')] bg-cover bg-center bg-no-repeat w-3/5 h-96 mb-7 rounded-lg mr-5"></div>
+                <div class="w-2/5 text-[#3a020d]">
+                    <h2 class="mb-1 text-3xl font-bold">${name}</h2>
+                    <h3 class="mb-4 text-xl">Publicado el: ${publicationDate}</h3>
+                    <p>Serie: ${anime}</p>
+                    <p class="mb-4 text-2xl">${description}</p>
+                    <h4 class="mb-4 text-xl">Requisitos:</h4>
+                    <p>Años de experiencia requeridos: ${experience}</p>
+                    <p>Ubicación: ${location}</p>
+                    <p>Solicitudes: ${applications}</p>
+                    <div class="flex justify-end mt-5">
+                        <button class="bg-[#ffe4ed] py-2 px-3 mr-3 text-xl rounded-lg font-semibold">Editar</button>
+                        <button class="bg-[#ce4164] py-2 px-3 text-xl text-white rounded-lg font-semibold">Eliminar</button>
+                    </div>
+                </div>
+                
+            </div>
+`
 }
 
 // *********************************** Events ***********************************
