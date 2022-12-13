1464dff0cb7b4545e197d9972e165ae973b3f3aa
july8712 <julietabondi@gmail.com>
2022-12-12 23:35:28 -0300

    Layout of details section

--- js/app.js
+++ js/app.js
@@ -229,18 +229,18 @@ const printDetails = (jobs) => {
     const { name, description, location, experience, salary, publicationDate, image, anime, page, applications, id } = jobs
 
     $("#details").innerHTML = `
-        <div class="w-3/5 bg-white p-10 flex border-2">
-                <div class="bg-[url('${image}')] bg-cover bg-center bg-no-repeat w-3/5 h-96 mb-7 rounded-lg mr-5"></div>
+        <div class="w-3/5 bg-white p-10 flex border-2 rounded">
+                <div class="bg-[url('${image}')] bg-cover bg-center bg-no-repeat w-3/5 h-[100%] mb-7 rounded-lg mr-5"></div>
                 <div class="w-2/5 text-[#3a020d]">
                     <h2 class="mb-1 text-3xl font-bold">${name}</h2>
-                    <h3 class="mb-4 text-xl">Publicado el: ${publicationDate}</h3>
-                    <p>Serie: ${anime}</p>
-                    <p class="mb-4 text-2xl">${description}</p>
-                    <h4 class="mb-4 text-xl">Requisitos:</h4>
-                    <p>Años de experiencia requeridos: ${experience == "Sin experiencia" ? "Sin experiencia" : (experience.slice(1,2) +"+ años")}</p>
-                    <p>Ubicación: ${location}</p>
-                    <p>Sueldo: $${salary}</p>
-                    <p>Solicitudes: ${applications}</p>
+                    <h3 class="mb-4 text-xl mb-5">Publicado el: ${publicationDate}</h3>
+                    <p class="text-2xl font-semibold mb-3">Serie: ${anime}</p>
+                    <p class="mb-4 text-xl">${description}</p>
+                    <h4 class="mb-4 text-2xl font-semibold mb-3">Requisitos:</h4>
+                    <p class="text-xl">Experiencia requerida: ${experience == "Sin experiencia" ? "Sin experiencia" : (experience.slice(1,2) +"+ años")}</p>
+                    <p class="text-xl">Ubicación: ${location}</p>
+                    <p class="text-xl">Sueldo: $${salary}</p>
+                    <p class="text-xl">Solicitudes: ${applications}</p>
                     <div class="flex justify-end mt-5">
                         <button class="bg-[#ffe4ed] py-2 px-3 mr-3 text-xl rounded-lg font-semibold" onclick="edit('${id}')">Editar</button>
                         <button class="bg-[#ce4164] py-2 px-3 text-xl text-white rounded-lg font-semibold" onclick="btnDelete('${id}')">Eliminar</button>
